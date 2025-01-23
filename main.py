c01386bce228fb8451b2551e188b8f5f158e5ec8
icodeologist <denzilpinto01@gmail.com>
2025-01-23 12:57:32 +0530

    Report print how man alphabets and their count

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from re import split
-
-
 def main():
     file_path = "../BookBot/books/frankenstein.txt"
     file_content = file_to_read(file_path)
     length , words = count_words(file_content)
     
     char_counter = count_ch_in_book(file_content)
-    print(char_counter)
-
+    char_counter = dict(sorted(char_counter.items(),key= lambda item: item[1], reverse=True))
+    
+    #print report
+    print_report(char_counter, length)
+    
 
 def file_to_read(path):
     with open(path) as f:
@@ -30,14 +30,17 @@ def count_ch_in_book(string):
             char_counter[j] = 1
     return char_counter
 
-
-
-
-
-
-
-
-
-
-
+def print_report(ans, total_words):
+    only_alphas = {}
+    for i in ans:
+        if i.isalpha():
+            only_alphas[i] = ans[i]
+    
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{total_words} words found in the document \n")
+    
+    for i in only_alphas:
+        print(f"The '{i}' character was found {only_alphas[i]} times")
+    print("--- End report ---")
+    
 main()
